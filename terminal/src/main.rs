51efc99b325447af75d0e290cc49f0ad9a2bfb97
Camus Qiu <camus@aion.network>
2019-02-19 18:06:52 +0800

    add naive terminal

---
+++ terminal/src/main.rs
@@ -0,0 +1,45 @@
+use std::io::{self, Write};
+use std::thread;
+use std::sync::mpsc::{channel, Sender};
+
+fn start(tx: Sender<i32>, info: String) {
+    match thread::Builder::new().name("cmd".to_string()).spawn(move || {
+        loop {
+            print!("{}", info);
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                if n == 0 {
+                    tx.send(0).unwrap();
+                    break;
+                }
+                input.pop();
+                match input.as_str() {
+                    _ => {
+                        println!("{} bytes read", n);
+                        println!("{}", input);
+                    },
+                }
+            }
+            Err(error) => println!("error: {}", error),
+            }
+        }
+    }) {
+        Ok(_) => println!("new thread cmd"),
+        Err(x) => println!("Create thread {} failed: {:?}", "cmd", x),
+    }
+}
+
+fn main() {
+    // commandline info
+    let cmd_line = "kernel-v0.1.0 >> ".to_string();
+
+    let (tx, rx) = channel::<i32>();
+    start(tx, cmd_line);
+
+    // wait for thread signals
+    let r = rx.recv().unwrap();
+
+    println!("main exits: {:?}", r);
+}
