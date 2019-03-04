8d9b655189bd786c4ce1fb43c4d0b854de7b3062
Camus Qiu <camus@aion.network>
2019-03-04 16:57:18 +0800

    feature: implement standalone Aion VM account module

---
+++ accounts/src/lib.rs
@@ -0,0 +1,20 @@
+extern crate acore_bytes as bytes;
+/// this is a standalone account model for Aion Blockchain
+///
+extern crate aion_types;
+extern crate lru_cache;
+
+#[macro_use]
+extern crate log;
+extern crate blake2b;
+extern crate rlp;
+#[macro_use]
+extern crate rlp_derive;
+extern crate db as kvdb;
+extern crate patricia_trie as trie;
+
+mod accounts;
+mod traits;
+mod account_db;
+
+pub use accounts::{FVMAccount, AVMAccount};
